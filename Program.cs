637363e89a9066960a72efd16656405bf2998d18
Margustova <shchennikova.ma@gmail.com>
2022-10-17 17:26:19 +0200

    Task4

--- Program.cs
+++ Program.cs
@@ -100,3 +100,19 @@ if(number % 2 == 0)
 }
 else 
     Console.WriteLine(($"{number} is not even")); */
+
+/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while(current < number)
+{
+    if(current % 2 == 0)
+    Console.Write($"{current},  ");
+    current++;
+}
+    Console.Write($"->{number}"); */
