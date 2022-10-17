6c1bfcc50213e440b193cc4d72f1d8c4cc904ce5
Margustova <shchennikova.ma@gmail.com>
2022-10-17 16:31:25 +0200

    task 2

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ Console.WriteLine($"Last digit of {number} is {result}");
 */
 
 //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.Write("Input a first number: "); 
+/*Console.Write("Input a first number: "); 
 int num1= Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: "); 
 int num2= Convert.ToInt32(Console.ReadLine());
@@ -51,9 +51,39 @@ else
 {
     max = num2;
 }
-Console.WriteLine($"num1={num1}, num2 = {num2}, -> max ={max}");
+Console.WriteLine($"num1={num1}, num2 = {num2}, -> max ={max}"); */
 
 /*a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
+
+/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 
+*/
+/* Console.Write("Input a first number: "); 
+int num1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: "); 
+int num2= Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3= Convert.ToInt32(Console.ReadLine()); 
+int max = num1;
+if(num1 < num2)
+{ 
+    max = num2;
+}
+if(num2 < num3)
+{
+    max = num3;
+}
+if(num1 > num3)
+{
+    max = num1;
+}
+Console.WriteLine($"num1={num1}, num2 = {num2}, num3={num3} -> max ={max}"); */
+
+
+
