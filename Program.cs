e3fc9d9e62a928091dc9327b9d0cc45dcab8dabf
Margustova <shchennikova.ma@gmail.com>
2022-10-22 16:54:58 +0200

    HW13

--- Program.cs
+++ Program.cs
@@ -72,5 +72,35 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("input n: ");
-int n = Convert.ToInt32(Console.ReadLine());Console.WriteLine(Exam(a, m, n)); */
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Exam(a, m, n)); */
 
+/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int Digit(int ThreeDigitNumber)
+{
+    int result = (ThreeDigitNumber / 10) % 10;
+        return result;
+}
+Console.Write("input ThreeDigitNumber: ");
+int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Digit(ThreeDigitNumber));
+
+
+/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
